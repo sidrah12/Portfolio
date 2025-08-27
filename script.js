a03da54d1cffbee7be0e42ea6dec060ee9c24233
sidrah12 <sidrahansari20@gmail.com>
2025-08-27 21:11:38 +0530

    Final

---
+++ script.js
@@ -0,0 +1,9 @@
+// Initialize Bootstrap tooltips for all elements with data-bs-toggle="tooltip"
+document.addEventListener('DOMContentLoaded', function () {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  tooltipTriggerList.forEach(function (tooltipTriggerEl) {
+    new bootstrap.Tooltip(tooltipTriggerEl);
+  });
+});
+
+//Project CARD
